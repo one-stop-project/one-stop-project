98035cb1767a644e23065447629a65bacb565d0f
Jeong Jihun <doksakim7@naver.com>
2026-05-12 20:12:10 +0900

    init: spring boot project setup
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/spartafarmer/one_stop/OneStopApplication.java
@@ -0,0 +1,13 @@
+package com.spartafarmer.one_stop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneStopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spartafarmer/one_stop/OneStopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartafarmer.one_stop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneStopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
