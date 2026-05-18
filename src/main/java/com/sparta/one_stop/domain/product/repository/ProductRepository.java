202c922d9396da0c633d14f1e8d2a81de71dbbff
eunjiom <norgg021@gmail.com>
2026-05-18 19:38:02 +0900

    [feat] 상품 승인/반려 API 구현 (#54)
    * [feat] 상품 승인/반려 API 구현
    
    * [feat] 상품 승인/반려 API 구현 및 에러코드/페이징/중복코드 개선

---
+++ src/main/java/com/sparta/one_stop/domain/product/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.sparta.one_stop.domain.product.repository;
+
+import com.sparta.one_stop.domain.product.entity.Product;
+import com.sparta.one_stop.global.enums.product.ProductStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // 상태별 상품 목록 조회 (관리자 승인/반려 목록용)
+    List<Product> findAllByStatus(ProductStatus status);
+
+    // 상태별 상품 목록 조회 - 페이징 (관리자 승인 대기 목록용)
+    Page<Product> findAllByStatus(ProductStatus status, Pageable pageable);
+}
