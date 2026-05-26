de76dcea37231eb8074c24bb8102dc38a88a68b6
ljh <leejunghyun1019@gmail.com>
2026-05-26 19:20:14 +0900

    [refactor] category에 (parent_id, name) 복합 인덱스 추가 #144

--- src/main/java/com/sparta/one_stop/domain/product/entity/Category.java
+++ src/main/java/com/sparta/one_stop/domain/product/entity/Category.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -16,7 +17,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "category")
+@Table(name = "category",
+        indexes = {
+                @Index(name = "idx_category_parent_name", columnList = "parent_id, name")
+        })
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category extends BaseEntity {
