fa4886ffe0693e5959fb45a1e3f4301c53a3e29b
Hojin-LIM1 <limhojin956@naver.com>
2026-06-05 19:43:15 +0900

    [refactor] set point domain retry (#254)
    * feat : user,seller Entity ì 추가, fix : ㅕjwtTokenProvider 수정
    
    * fix:일부 수정사항 반영 ; changepassword제거, jwt_secret 수정
    
    * feat : auth ; refreshToken운용을 위한 Filter(인증, 예외), entryPoint, CustomUserDetails 구현
    
    * yml 파일 수정
    
    * chore: 테스트 환경 JWT 시크릿 키 기본값 추가
    
    * rebuild
    
    * rebiuld2
    
    * fix:AuthUser 방식 추가
    
    * rebuild2
    
    * Authfix:User 책임 강í화
    
    * fix : SecurityConfig 수정, feat:AccessDenied Handler 구현
    
    * fix : FE분기 고려 AccessDeniedHandler 변경
    
    * fix : deadCode제거, jwtTokenProvider에서 try Catch문 제ê±
    
    * feat : ëAuth 도메인 구현
    
    * feat : ëAuthA메인 구현
    
    * check
    
    * feat : 회원 도메인 구현
    
    * redis ì설정 완료
    
    * fix : 빈메서드 삭제, swagger 적용
    
    * 추가 수정
    
    * test : 테스트 코드 작ì(23case) / fix:코드래빗 언어 수정
    
    * feat : OAuth2구현 ,DDD 등
    
    * fix:test code 수정
    
    * refactor : Auth
    
    * fix : luaScript Parsing problem 해결
    
    * feat : user Status Redis Caching
    
    * feat : CORS 설정(FE연동)
    
    * fix : test.yml ã파일 수정
    
    * fix : 리뷰 수정
    
    * auth#158
    
    * ëfix : value 수정(swagger 사용 가능 fall back 설정)
    
    * í리뷰 수정
    
    * feat : FE 개발
    
    * FE 수정
    
    * test : auth,user Service Test
    
    * feat :  보안감사 로그 기능 추가
    
    * refactor : pointDomain 보안, 무결성, 정합성, 강화
    
    * refactor : pointDomain 보안, 무결성, 정합성, 강화
    
    * ììë¦fix : 시간 동기화, sql 인젝션, 검증 로직 등 수정
    
    * ífix : 오타 수정
    
    * ífix : 오타 수ì 2
    
    * ìfix : 수ìyml, testporfile 수ì정완료
    
    * 재수정
    
    * 재수정
    
    B
    
    * fix : 중복 bean 삭제
    
    * fix : redis 재설정
    
    * fix : RedissonConfig 추가
    
    * test : SellerTest추가
    
    * fix : 리뷰 반영
    
    * fix : 리뷰 반영2
    
    * fix : point retry

--- src/main/java/com/sparta/one_stop/OneStopApplication.java
+++ src/main/java/com/sparta/one_stop/OneStopApplication.java
@@ -3,12 +3,14 @@ package com.sparta.one_stop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // JPA Auditing 활성화 (BaseEntity의 @CreatedDate, @LastModifiedDate 동작)
 // 스케줄러 활성화 (조회수 Redis → DB 동기화 등)
 @EnableJpaAuditing
 @EnableScheduling
+@EnableRetry
 @SpringBootApplication
 public class OneStopApplication {
 
